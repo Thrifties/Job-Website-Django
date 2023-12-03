90de74e3a3e219c05d82ed3a1afd347c96054376
Paulo-Regalado <paulosregalado@gmail.com>
2023-12-03 14:34:21 +0800

    Dashboard

--- job_website/employer/views.py
+++ job_website/employer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def dashboard(request):
+
+    template = 'dashboard.html'
+    context = {
+        'title': 'Dashboard Page'
+    }
+    return render(request, template, context)
