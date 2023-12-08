d8ab7a19a1045d350cc5564e797bac00e45ec1cb
SourTamarind <paolojenesis.meneses.v@bulsu.edu.ph>
2023-12-08 11:02:46 +0800

    Company profile

--- job_website/employer/urls.py
+++ job_website/employer/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import get_company_data
 from .views import update_company_profile
 from .views import update_profile_picture
+from .views import update_cover_photo
 from .views import view_resume, applicant_list, approve_applicant, reject_applicant
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,7 +22,8 @@ urlpatterns = [
     path('profile_settings', views.profile_settings, name="profile_settings"),
     path('get_company_data/<int:company_id>/', get_company_data, name='get_company_data'),
     path('update_company_profile/<int:company_id>/', update_company_profile, name='update_company_profile'),
-    path('update_profile_picture/', update_profile_picture, name='update_profile_picture')
+    path('update_profile_picture/', update_profile_picture, name='update_profile_picture'),
+    path('update_cover_photo/', update_cover_photo, name='update_cover_photo'),
     path('login', views.login, name='login'),
     path('to_login', views.to_login, name='to_login'),
     path('add_job', views.add_job, name="add_job"),
