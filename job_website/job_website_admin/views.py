fef779ce1ef2fb5083a55cdbc90765cb63ffe0fb
Thrifties <90311744+Thrifties@users.noreply.github.com>
2023-12-08 14:15:29 +0800

    Add first name to dashboard_admin template

--- job_website/job_website_admin/views.py
+++ job_website/job_website_admin/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from employer.models import Job, Applicant, Details
 
 # Create your views here.
 
 def dashboard_admin(request):
+    first_name = Details.objects.get().first_name
     template = 'dashboard_admin.html'
-    return render(request, template)
+    context = {
+        'first_name': first_name,
+    }
+    return render(request, template, context)
 
 def admin_list_of_jobs(request):
     template = 'admin_list_of_jobs.html'
