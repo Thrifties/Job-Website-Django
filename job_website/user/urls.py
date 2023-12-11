daab30dae81c3f7bfd2b15ffccc772bf80392f5f
Thrifties <90311744+Thrifties@users.noreply.github.com>
2023-12-11 13:18:11 +0800

    Add User and Employee models, update navbar CSS, and modify login functionality

--- job_website/user/urls.py
+++ job_website/user/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('homepage', views.homepage, name='homepage'),
+    path('user_register', views.user_register, name='user_register'),
+    path('add_user', views.add_user, name='add_user'),
+    path('user_login', views.user_login, name='user_login'),
+    path('user_toLogin', views.user_toLogin, name='user_toLogin'),
 ]
--- job_website/user/views.py
+++ job_website/user/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User as AuthUser
+from django.contrib.auth.hashers import make_password, check_password
+from .models import User, Employee
 
-# Create your views here.
+def user_register(request):
+    template = 'user_register.html'
+    context = {
+        'title' : 'User Register',
+    }
+    return render(request, template, context)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        auth_user = AuthUser.objects.create_user(username=email, email=email, password=password)
+        auth_user.save()
+        
+        Employee.objects.create(
+            email=email,
+            password=make_password(password),
+        )
+        
+        return redirect('user_login')
+    else:
+        return redirect('user_register')
+    
+def user_login(request):
+    template = 'user_login.html'
+    context = {
+        'title' : 'User Login',
+    }
+    return render(request, template, context)
+
+
+def user_toLogin(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = Employee.objects.get(email=email)
+        if check_password(password, user.password):
+            return redirect('homepage')
+        else:
+            return redirect('user_login')
+    else:
+        return redirect('user_login')
 
 def homepage(request):
     template = 'user_homepage.html'
